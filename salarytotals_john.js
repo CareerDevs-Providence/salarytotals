10b14c81f7261717481bd608a723d0e76c889f47
dinohologram <42440043+dinohologram@users.noreply.github.com>
2019-12-05 12:40:57 -0500

    Additional Features

--- salarytotals_john.js
+++ salarytotals_john.js
@@ -6,25 +6,80 @@ departmentEmployees = []
 
 departmentSalaries = []
 
+personalInfo =[]
+
+var arg = process.argv.slice(2) //look at switch for possible arguments
+
 //------------------------------------------------------------------------------------
 
-departments()
+loadDepartments() //load* takes SQL into our arrays 
 
-employees()
+loadEmployees()
 
-salaries()
+loadSalaries()
 
-displayCurrentTotals()
+loadPersonalInfo()
+
+switch(arg[0]) {  //takes arguments so usage can be like 'node salarytotals.js department 5' or 'node salarytotals.js employee 10001'
+
+    case 'totals':
+
+        displayCurrentTotals()
+        break
+
+    case 'department':
+
+        let department = arg[1] - 1
+
+        let current = arg[2]
+
+        if (current == 'current') { 
+
+            console.log(departmentNames[department][1])
+
+            console.log(currentEmployees(department))
+
+        } 
+            else {
+
+            console.log(departmentNames[department][1])
+
+            console.log(departmentEmployees[department])
+        }
+        break
+        
+
+    case 'employee':
+
+        let employeeID = arg[1]
+
+        console.log(employeeLookup(employeeID))
+
+        console.log("SALARY HISTORY")
+
+        console.log(salaryTracker(employeeID))
+
+        break
+
+    default:
+
+	    displayCurrentTotals()
+    
+        break
+
+
 
 //console.log(departmentSalaries) 
 
-//console.log(departmentEmployees[x])
 
-function departments() {
+
+}
+
+function loadDepartments() {
 
     const data = fs.readFileSync('load_dept_names.txt','utf-8')
 
-        fArray = data.replace(/'|\(|\)|;|/g, "")
+        let fArray = data.replace(/'|\(|\)|;|/g, "")
         fArray = fArray.split('\n')
 
         for (let x =0; x < fArray.length; x++) {
@@ -44,7 +99,7 @@ function departments() {
 
 }
 
-function employees() {
+function loadEmployees() {
     let data = fs.readFileSync('load_dept_emp.txt','utf-8')
 
     let pos = data.lastIndexOf('VALUES') + 7
@@ -80,7 +135,7 @@ function employees() {
 
 }
 
-function salaries() {
+function loadSalaries() {
 
     let data = fs.readFileSync('load_salaries.txt','utf-8')
 
@@ -108,10 +163,12 @@ function salaries() {
         departmentSalaries[x] = []
     }
 
+    currentEmps = currentEmployees('all')
+
     for (let x =0; x < fArray.length; x++) {                  
         for (let i = 0; i < departmentNames.length; i++) {
-            for (let j = 0; j < departmentEmployees[i].length; j++) {
-                if (fArray[x][0] == departmentEmployees[i][j][0]) {
+            for (let j = 0; j < currentEmps[i].length; j++) {
+                if (fArray[x][0] == currentEmps[i][j][0]) {
                     departmentSalaries[i].push(fArray[x])
                 }
             }
@@ -122,23 +179,95 @@ function salaries() {
 }
 
 function displayCurrentTotals() {
-    console.log("CURRENT TOTALS:")
+    console.log("\nCURRENT DEPARTMENT SALARY TOTALS:\n")
     var total = 0
+    var grand_total = 0
 
     for (let x = 0; x < departmentSalaries.length; x++) {
         for (let y = 0; y < departmentSalaries[x].length; y++) {
             if (departmentSalaries[x][y][3] == '9999-01-01') {
-
                 total += parseInt(departmentSalaries[x][y][1])
 
             }
         }
 
-        
-    console.log(departmentNames[x][1] + " " + total) 
+    grand_total += total    
+    console.log("\t" + departmentNames[x][1] + " " + total) 
     total = 0  
 
     }
+    console.log("\nGRAND TOTAL: " + grand_total + "\n" )
 
 }
 
+function loadPersonalInfo() {
+
+    let datab = fs.readFileSync('load_employee.txt','utf-8')
+
+    let pos = datab.lastIndexOf('VALUES') + 7
+    let length = datab.length
+    datab = datab.substr(pos, length)
+
+    datab = datab.replace(/'|\(|\)|;|/g, "")
+
+    let gArray = datab.split('\n') //array of all employees personal info
+    
+
+    for (let x =0; x < gArray.length; x ++) {
+        gArray[x] = gArray[x].replace(/,$/,'')
+        gArray[x] = gArray[x].split(',')
+        personalInfo.push(gArray[x])
+    }
+
+
+}
+
+function employeeLookup(employeeID) {
+
+    
+
+    for (let x = 0; x < personalInfo.length; x++) {
+        if (employeeID == personalInfo[x][0]) {
+            return personalInfo[x]
+        }
+    } 
+
+}
+
+function salaryTracker(employeeID) {
+
+    var salaryHistory = []
+
+    for (let x = 0; x < departmentSalaries.length; x++) {
+        for (let y = 0; y < departmentSalaries[x].length; y++) {
+            if (employeeID == departmentSalaries[x][y][0]) {
+                salaryHistory.push(departmentSalaries[x][y])          
+            }
+        }
+    }
+
+    return salaryHistory
+}
+
+function currentEmployees(dep) {
+
+    var currentEmps = []
+
+    for (let x = 0; x < departmentEmployees.length; x++) {
+        currentEmps[x] = []
+    }
+    
+    regEx = /9999/
+
+        for (let x = 0; x < departmentEmployees.length; x++ ) {
+            for (let y = 0; y < departmentEmployees[x].length; y++) {
+                if (regEx.test(departmentEmployees[x][y][3])) {
+                    currentEmps[x].push(departmentEmployees[x][y])
+                }
+            }
+        }
+    
+    if (dep == 'all') { return currentEmps}
+
+    return currentEmps[dep]
+}
